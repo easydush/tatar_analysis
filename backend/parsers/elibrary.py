e9ba5e01034e4c9e0eb9b65c5ff57fde21b96133
easydush <easydush@gmail.com>
2022-05-23 01:08:38 +0300

    updated parser

--- backend/parsers/elibrary.py
+++ backend/parsers/elibrary.py
@@ -1,11 +1,16 @@
+import time
+
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from decouple import config
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 SEARCH_QUERIES = ['Академия наук Республики Татарстан']
-PDF_LINKS = []
 driver = webdriver.Chrome()
-
+wait = WebDriverWait(driver, 10)
+pdf_file = open("pdfs.txt", "w")
 
 def authorize():
     driver.get('http://www.elibrary.ru')
@@ -25,27 +30,48 @@ def get_anrt_page():
 
 
 def extract_links_from_page():
-    return driver.find_elements(By.XPATH, '//a[starts-with(@href,"/item.asp")]')
+    elems = driver.find_elements(By.XPATH, '//a[starts-with(@href,"/item.asp")]')
+    return [elem.get_attribute('href') for elem in elems]
 
 
-def exctract_all_links():
-    links = extract_links_from_page()
-    return links
+def extract_all_links():
+    all_links = extract_links_from_page()
+
+    next_page = driver.find_element(By.XPATH, '//a[text()=">>"]')
+    while next_page:
+        next_page.click()
+        all_links += extract_links_from_page()
+        try:
+            next_page = driver.find_element(By.XPATH, '//a[text()=">>"]')
+        except NoSuchElementException:
+            return all_links
 
 
 def parse_page(link):
-    link.click()
-    file = driver.find_element(By.XPATH, '//*[text()="Полный текст"]')
+    driver.get(link)
+    try:
+        file = driver.find_element(By.XPATH, '//*[starts-with(text(),"Полный текст")]')
+    except NoSuchElementException:
+        return
     file.click()
-    print(driver.current_url)
-    new_window =driver.current_window_handle
-    PDF_LINKS.append(driver.current_url)
+    pdf_file.write(f'{driver.current_url}\n')
+    time.sleep(5)
 
 
 if __name__ == '__main__':
     authorize()
-    get_anrt_page()
-    links = exctract_all_links()
+    #
+    # get_anrt_page()
+    # links = extract_all_links()
+    #
+    # with open("links.txt", "w") as txt_file:
+    #     for link in links:
+    #         txt_file.write(f'{link}\n')
+
+    links = []
+
+    with open("links.txt", "r") as txt_file:
+        links = txt_file.readlines()
 
     for link in links:
-        parse_page(link)
+        parse_page(link.replace('\n', ''))
