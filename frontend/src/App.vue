02673e9208e1e599957378e142d6a63da64a2040
easydush <easydush@gmail.com>
2022-06-10 00:11:59 +0300

    inited frontend app

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Header msg="tatar analysis"/>
+</template>
+
+<script>
+import HeaderComponent from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header: HeaderComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
