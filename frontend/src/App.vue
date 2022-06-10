8b7c66b935f29d2f320e9574b2bc8bc1f6e2aaa8
easydush <easydush@gmail.com>
2022-06-11 02:30:48 +0300

    updated frontend app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Header msg="tatar analysis"/>
+    <div class="common-layout">
+        <el-container>
+            <el-header>
+                <Header msg="tattext" />
+            </el-header>
+
+            <el-main>
+                <el-input
+                    v-model="input"
+                    autosize
+                    type="textarea"
+                    placeholder="Введите текст"
+                    clearable
+                />
+            </el-main>
+
+            <el-row class="mb-4" justify="end">
+                <el-button type="primary">Отправить</el-button>
+            </el-row>
+        </el-container>
+    </div>
 </template>
 
 <script>
-import HeaderComponent from './components/Header'
+import { ElContainer, ElHeader, ElMain, ElInput } from 'element-plus';
+import HeaderComponent from './components/Header';
 
 export default {
-  name: 'App',
-  components: {
-    Header: HeaderComponent
-  }
-}
+    name: 'App',
+    components: {
+        ElContainer,
+        ElHeader,
+        ElMain,
+        ElInput,
+        Header: HeaderComponent,
+    },
+    data() {
+        return {
+            input: null,
+        };
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
